b28908e570bf261529eb6d9f6cc022e6ce70454d
Ramesh Polishetti <rpolishe@tibco.com>
2025-07-23 17:00:43 +0530

    Print app version during startup

--- src/main/java/com/csg/demo/DemoApp.java
+++ src/main/java/com/csg/demo/DemoApp.java
@@ -1,5 +1,9 @@
 package com.csg.demo;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -7,6 +11,16 @@ public class DemoApp {
     private static final Logger logger = LogManager.getLogger(DemoApp.class);
 
     public static void main(String[] args) {
+        try {
+            InputStream is = DemoApp.class.getClassLoader()
+                    .getResourceAsStream("META-INF/maven/com.csg/demo-app/pom.properties");
+            Properties props = new Properties();
+            props.load(is);
+            String version = props.getProperty("version");
+            logger.info("Demo Application Version: {}", version);
+        } catch (IOException e) {
+            logger.error("Error reading pom.properties file", e);
+        }
         logger.info("Application started.");
         logger.debug("This is a debug message.");
         logger.warn("This is a warning.");
